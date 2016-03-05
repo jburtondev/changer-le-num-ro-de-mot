6242d499580fd2d3c015e92af331c05b81424e98
jburtondev <jamiejburton@hotmail.co.uk>
2016-03-05 17:20:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# changer le numéro de mot
+A simple program which converts a number into its string equivalent.
+
+Uses Angularjs, HTML & CSS.
---
+++ js/data.js
@@ -0,0 +1,41 @@
+//Holds data for number outputs
+(function(angular) {
+
+    App.Data = {};
+
+    App.Data.ones = [
+        '',
+        'one ',
+        'two ',
+        'three ',
+        'four ',
+        'five ',
+        'six ',
+        'seven ',
+        'eight ',
+        'nine ',
+        'ten ',
+        'eleven ',
+        'twelve ',
+        'thirteen ',
+        'fourteen ',
+        'fifteen ',
+        'sixteen ',
+        'seventeen ',
+        'eighteen ',
+        'nineteen '
+    ];
+    App.Data.tens = [
+        '',
+        '',
+        'twenty',
+        'thirty',
+        'forty',
+        'fifty',
+        'sixty',
+        'seventy',
+        'eighty',
+        'ninety'
+    ];
+
+})(window.angular);
